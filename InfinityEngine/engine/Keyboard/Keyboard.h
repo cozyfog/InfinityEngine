fa1fd7b425c1adcc47b6ba448a29b6e334c19429
nickel <92646300+nickel-dev@users.noreply.github.com>
2023-05-07 11:44:18 -0700

    Delete InfinityEngine/engine directory

--- InfinityEngine/engine/Keyboard/Keyboard.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <base/typedef.h>
-#include <GLFW/glfw3.h>
-
-namespace Keyboard {
-	bool isPressed(i32 key);
-	void callback(GLFWwindow* window, i32 key, i32 scancode, i32 action, i32 mods);
-};
